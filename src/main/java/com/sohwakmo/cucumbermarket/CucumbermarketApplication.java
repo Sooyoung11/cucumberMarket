d66bc2941795bcfd52735664fba3c2f95f8152bb
swim <sijkok@naver.com>
2022-11-28 13:40:54 +0900

    start project

---
+++ src/main/java/com/sohwakmo/cucumbermarket/CucumbermarketApplication.java
@@ -0,0 +1,13 @@
+package com.sohwakmo.cucumbermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumbermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumbermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sohwakmo/cucumbermarket/CucumbermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohwakmo.cucumbermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumbermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
