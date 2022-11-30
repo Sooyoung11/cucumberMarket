ac4fb863ecd4ffc20ffe3e7f7421076b4eba9125
swim <sijkok@naver.com>
2022-11-30 19:15:39 +0900

    11-30 수영..

--- src/main/java/com/sohwakmo/cucumbermarket/controller/ProductController.java
+++ src/main/java/com/sohwakmo/cucumbermarket/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.sohwakmo.cucumbermarket.controller;
 
+import com.sohwakmo.cucumbermarket.domain.Product;
 import com.sohwakmo.cucumbermarket.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,10 +20,15 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/list")
-    public void list() {
+    public String list(Model model) {
         log.info("list()");
 
+        List<Product> list = productService.read();
+        model.addAttribute("list", list);
 
+        log.info("list={}", list);
+
+        return "/product/list";
     }
 
     @GetMapping("/detail")
--- src/test/java/com/sohwakmo/cucumbermarket/ProductSwimTest.java
+++ src/test/java/com/sohwakmo/cucumbermarket/ProductSwimTest.java
@@ -1,15 +1,20 @@
 package com.sohwakmo.cucumbermarket;
 
+import com.sohwakmo.cucumbermarket.domain.Product;
 import com.sohwakmo.cucumbermarket.repository.ProductRepository;
 
 
-
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 
 @SpringBootTest
+@Slf4j
 public class ProductSwimTest {
 
     @Autowired
@@ -17,9 +22,14 @@ public class ProductSwimTest {
 
     @Test
     public void test1() {
-        System.out.println("test1()");
+        log.info("test1()");
 
+        Assertions.assertNotNull(productRepository);
 
+        List<Product> list = productRepository.findAll();
+        for (Product p : list) {
+            log.info(p.toString());
+        }
     }
 
 }
