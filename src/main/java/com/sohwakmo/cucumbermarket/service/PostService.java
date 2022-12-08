95c0709bbbefce37d07b914498d0a01e4608bc8f
ByeonJuHwan <asdcz@naver.com>
2022-12-08 13:59:51 +0900

    ver1.0 -> 포스트 수정완료 01

--- src/main/java/com/sohwakmo/cucumbermarket/service/PostService.java
+++ src/main/java/com/sohwakmo/cucumbermarket/service/PostService.java
@@ -41,8 +41,7 @@ public class PostService {
         List<Post> postList =  postRepository.findByTitleIgnoreCaseContainingOrContentIgnoreCaseContainingOrderByPostNoDesc(searchText,searchText);
         List<PostReadDto> list = new ArrayList<>();
         for(Post p : postList){
-            // TODO: getId 값 수정하기
-            Member member = memberRepository.findById(1).get();
+            Member member = memberRepository.findById(p.getMember().getMemberNo()).get();
             PostReadDto dto = PostReadDto.builder()
                     .postNo(p.getPostNo()).title(p.getTitle()).writer(member.getNickname()).createdTime(p.getCreatedTime()).clickCount(p.getClickCount())
                     .build();
