53dcbb4a7fcaf3caae6330dab323efd727184980
eunsuu <eunsuu7@gmail.com>
2022-12-14 16:04:07 +0900

    주환자리로 이동

--- src/main/java/com/sohwakmo/cucumbermarket/controller/HomeController.java
+++ src/main/java/com/sohwakmo/cucumbermarket/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.sohwakmo.cucumbermarket.controller;
 
+import com.sohwakmo.cucumbermarket.domain.Post;
 import com.sohwakmo.cucumbermarket.domain.Product;
+import com.sohwakmo.cucumbermarket.service.PostService;
 import com.sohwakmo.cucumbermarket.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -16,11 +19,27 @@ import java.util.List;
 public class HomeController {
 
     private final ProductService productService;
+    private final PostService postService;
     @GetMapping("/")
     public String index(Model model) {
         log.info("index()");
-        List<Product> productList= productService.readByLikeCountDesc();
+        List<Product> list= productService.readByLikeCountDesc();
+        List<Product> productList= new ArrayList<>();
+        for(Product p: list){
+            if(productList.size()<8){
+                productList.add(p);
+            }
+        }
         model.addAttribute("productList", productList);
+
+        List<Post> list2= postService.readByClickCountDesc();
+        List<Post> postList= new ArrayList<>();
+        for(Post p: list2){
+            if(postList.size()<10){
+                postList.add(p);
+            }
+        }
+        model.addAttribute("postList", postList);
 
         return "index";
     }
