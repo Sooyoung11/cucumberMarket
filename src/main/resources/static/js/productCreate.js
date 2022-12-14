8ad3848d09b9701fcc652c12b193450b6313fed2
MinYss <saocwxx116@naver.com>
2022-12-14 11:06:03 +0900

    css하는중

--- src/main/resources/static/js/productCreate.js
+++ src/main/resources/static/js/productCreate.js
@@ -1,2 +1,25 @@
 // 파일 확장자
+function fileCheck(event) {
+    pathpoint = event.value.lastIndexOf('.');
+    filepoint = event.value.substring(pathpoint+1, event.length);
+    filetype=filepoint.toLowerCase();
+    if(filetype=='jpg'||filetype=='png'||filetype=='jpeg'){
+    } else {
+        alert('첨부파일은 jpg,png,jpeg로 된 이미지만 가능합니다.')
+        parentObj = event.parentNode
+        node = parentObj.replaceChild(event.cloneNode(true),event);
+        return false;
+    }
+
+    let i = event.target.files.length-1;
+    for(let image of event.target.files){
+        let img = document.createElement("img");
+        const reader = new FileReader();
+        reader.onload = function (event){
+            img.setAttribute("src",event.target.result);
+        }
+        reader.readAsDataURL(event.target.files[i--]);
+        document.querySelector("#imgUpload").appendChild(img);
+    }
+}
 
