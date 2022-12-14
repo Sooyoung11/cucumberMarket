8d92014172f63be75ae6a53894ef22ebacff188d
MinYss <saocwxx116@naver.com>
2022-12-14 18:03:11 +0900

    페이징 하는중

--- src/main/resources/static/js/productCreate.js
+++ src/main/resources/static/js/productCreate.js
@@ -4,15 +4,14 @@ function fileCheck(event) {
     filepoint = event.value.substring(pathpoint+1, event.length);
     filetype=filepoint.toLowerCase();
     if(filetype=='jpg'||filetype=='png'||filetype=='jpeg'){
+
+        return true;
     } else {
         alert('첨부파일은 jpg,png,jpeg로 된 이미지만 가능합니다.')
         parentObj = event.parentNode
         node = parentObj.replaceChild(event.cloneNode(true),event);
-        return;
+        return false;
     }
-
-
-
 
 
 
