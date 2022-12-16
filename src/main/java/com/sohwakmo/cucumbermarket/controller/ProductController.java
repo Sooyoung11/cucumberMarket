a0bb53572d0f4f2a5e00115d84260ef11899ab9e
MinYss <saocwxx116@naver.com>
2022-12-16 13:34:32 +0900

    페이징, ui

--- src/main/java/com/sohwakmo/cucumbermarket/controller/ProductController.java
+++ src/main/java/com/sohwakmo/cucumbermarket/controller/ProductController.java
@@ -22,7 +22,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.awt.*;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
